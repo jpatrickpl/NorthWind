3c3b53a428a07aca8dc445f857df353b5bce5ebb
patrick paipay <jpatrickpl@gmail.com>
2021-02-05 20:20:18 -0500

    f

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProductRoutingModule } from './product-routing.module';
+import { ProductMainContainerComponent } from './containers/product-main-container/product-main-container.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ProductMainContainerComponent],
   imports: [
     CommonModule,
     ProductRoutingModule
